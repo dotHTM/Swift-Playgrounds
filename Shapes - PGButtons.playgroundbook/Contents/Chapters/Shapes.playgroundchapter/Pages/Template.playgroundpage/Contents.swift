a6116f6e872213820261f2fda6cccef8e8fa2877
Mike Cramer <imanerd@me.com>
2018-01-24 12:15:47 -0600

    Simplified directories
    Tapping through multiple levels of directories is annoying on my iPad,
    so just cutting them all short until there's a need to organize

--- Shapes - PGButtons.playgroundbook/Contents/Chapters/Shapes.playgroundchapter/Pages/Template.playgroundpage/Contents.swift
+++ Shapes - PGButtons.playgroundbook/Contents/Chapters/Shapes.playgroundchapter/Pages/Template.playgroundpage/Contents.swift
@@ -1,4 +1,4 @@
-//#-code-completion(module, hide, Swift)
+
 //#-code-completion(identifier, hide, _setup())
 //#-code-completion(identifier, hide, AbstractDrawable)
 //#-code-completion(identifier, hide, _ColorLiteralType)
